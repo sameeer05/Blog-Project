fd1f25e0ed89f2f3035b699f4e9640399f45b78f
Sameer <sameeransari663@gmail.com>
2022-10-28 20:06:07 +0530

    Trying Again.

--- main.py
+++ main.py
@@ -62,7 +62,7 @@ class BlogPost(db.Model):
     __tablename__ = "blog_posts"
     id = db.Column(db.Integer, primary_key=True)
     author_id = db.Column(db.Integer, db.ForeignKey("users.id"))
-    author = relationship("User", back_populates="posts", lazy='subquery')
+    author = relationship("User", back_populates="posts")
     title = db.Column(db.String(250), unique=True, nullable=False)
     subtitle = db.Column(db.String(250), nullable=False)
     date = db.Column(db.String(250), nullable=False)
